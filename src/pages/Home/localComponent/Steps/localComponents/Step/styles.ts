91bc7efae47a83a0ed55673f2c5c6d550da641a3
João Victor <95556375+HomemJaguar111@users.noreply.github.com>
2022-04-22 00:13:49 -0300

    create component with animation

--- src/pages/Home/localComponent/Steps/localComponents/Step/styles.ts
+++ src/pages/Home/localComponent/Steps/localComponents/Step/styles.ts
@@ -1,10 +1,12 @@
-import { Props } from "styled-components/native";
+import { Props } from "./types";
 import styled from "styled-components/native";
+import Animated from "react-native-reanimated";
 
-export const Container = styled.View<Props>`
+export const Container = styled(Animated.View)<Props>`
   background-color: ${({ theme, isActive }) =>
     !isActive ? theme.colors.placeholder.main : theme.colors.background.onMain};
   height: 6px;
+
   border-radius: 3px;
   margin: 0 2.5px;
 `;
